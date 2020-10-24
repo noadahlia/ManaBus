82948775eb87e3eb1ac3a22668bd5ab500662785
Elisheva Wechsler <ewechsle@g.jct.ac.il>
2020-10-24 20:48:58 +0200

    Initial code + Welcome9212

---
+++ dotNet5781_00_7799_9212/Program9212.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_7799_9212
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome9212();
+            Welcome7799();
+            Console.ReadKey();
+        }
+        static partial void Welcome7799();
+        private static void Welcome9212()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
