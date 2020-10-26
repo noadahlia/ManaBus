561748598a841b8c3a1ca347b6e379eb7cd43810
noadahlia <ghozlan@g.jct.ac.il>
2020-10-26 15:08:16 +0100

    Initial code + Welcome7799

---
+++ dotNet5781_00_7799_9212/Program7799.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_7799_9212
+{
+    partial class Program
+    {
+        static partial void Welcome7799()
+        {
+            Console.WriteLine("Coucou Elishou!! :) ");
+            Console.WriteLine("Je ne comprend pas pk ca marche pas");
+        }
+    }
+}
