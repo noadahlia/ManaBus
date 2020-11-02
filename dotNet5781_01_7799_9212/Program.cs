5e0de6486f56a68de1348e02a7611216f82274ec
noadahlia <ghozlan@g.jct.ac.il>
2020-11-02 19:44:22 +0100

    Ex01 - first step - comments in french

---
+++ dotNet5781_01_7799_9212/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_7799_9212
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Main menu en boucle
+            // case 1: recuperer l id du bus + date de debut pour le rentrer dans la liste des bus
+            // case 2: choisir son bus. comment? en demandant le b_id 
+            // le programme donnera de facon aleatoire de kilometrage de la nessia 
+            // si b_id faux, on envoie un msg d erreur
+            // si b_id est bon, alors on enregistrera ce random dans les km parcourus par ce bus
+            // case 3: plein ou maintenance : cin b_id et cin plein ou maintenance
+            // si plein, alors maj un flag a true , flag qui montre s'il y a plein
+            // si maintenance, enregistrer la date actuelle avec datetime() et enregistrer le trajet dans lequel a ete fait ce tipoul
+            // case 4: afficher pour tous les bus leur trajet depuis le dernier tipoul
+            // pour chaque bus, il faut afficher le b_id et le trajet
+            // case 5: exit
+        }
+    }
+}
