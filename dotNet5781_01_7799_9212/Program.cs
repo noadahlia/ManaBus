7c06eea9861ffc7a045cdef7b3892ecce9054914
Elisheva Wechsler <ewechsle@g.jct.ac.il>
2020-11-03 18:07:00 +0100

    First change

--- dotNet5781_01_7799_9212/Program.cs
+++ dotNet5781_01_7799_9212/Program.cs
@@ -16,28 +16,29 @@ namespace dotNet5781_01_7799_9212
             Console.WriteLine("Press 3 to performe refueling or handling of a bus");
             Console.WriteLine("Press 4 to presente the traject since the last treatment for all vehicles in the company");
             Console.WriteLine("Press 5 to exit");
-            caseSwitch= Console.Read();
-            while (caseSwitch!=5)
+            caseSwitch = int.Parse(Console.ReadLine());
+
+            while (caseSwitch != 5)
             {
                 switch (caseSwitch)
                 {
                     case 1:  // case 1: recuperer l id du bus + date de debut pour le rentrer dans la liste des bus
-                        ;
+                        Console.WriteLine("1");
                         break;
                     case 2:  // case 2: choisir son bus. comment? en demandant le b_id 
-                             // le programme donnera de facon aleatoire de kilometrage de la nessia 
-                             // si b_id faux, on envoie un msg d erreur
-                             // si b_id est bon, alors on enregistrera ce random dans les km parcourus par ce bus
-                        ;
+                        //le programme donnera de facon aleatoire de kilometrage de la nessia
+                        //si b_id faux, on envoie un msg d erreur
+                        Console.WriteLine("1");   // si b_id est bon, alors on enregistrera ce random dans les km parcourus par ce bus
+
                         break;
                     case 3:  // case 3: plein ou maintenance : cin b_id et cin plein ou maintenance
-                             // si plein, alors maj un flag a true , flag qui montre s'il y a plein
-                             // si maintenance, enregistrer la date actuelle avec datetime() et enregistrer le trajet dans lequel a ete fait ce tipoul
-                        ;
+                        //si plein, alors maj un flag a true , flag qui montre s'il y a plein
+                        //      si maintenance, enregistrer la date actuelle avec datetime() et enregistrer le trajet dans lequel a ete fait ce tipoul
+                        Console.WriteLine("1"); ;
                         break;
                     case 4:      // case 4: afficher pour tous les bus leur trajet depuis le dernier tipoul
-                                 // pour chaque bus, il faut afficher le b_id et le trajet
-                        ;
+                        //pour chaque bus, il faut afficher le b_id et le trajet
+                        Console.WriteLine("1");
                         break;
                     default:
                         Console.WriteLine("Error, please enter a correct number");
@@ -48,18 +49,18 @@ namespace dotNet5781_01_7799_9212
                 Console.WriteLine("Press 3 to performe refueling or handling of a bus");
                 Console.WriteLine("Press 4 to presente the traject since the last treatment for all vehicles in the company");
                 Console.WriteLine("Press 5 to exit");
-                caseSwitch = Console.Read();
+                caseSwitch = int.Parse(Console.ReadLine());
 
-            } 
-            
-            
-            
-            
+            }
+
+
+
+
             //Main menu en boucle
-           
-            
-          
-   
+
+
+
+
         }
     }
 }
