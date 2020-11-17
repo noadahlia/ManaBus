fc310b2521e3fdeac1656507d9c683d911646c1b
noadahlia <ghozlan@g.jct.ac.il>
2020-11-17 01:05:40 +0100

    EX2 - BusLine class (begining)

---
+++ dotNet5781_02_7799_9212/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_7799_9212
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
