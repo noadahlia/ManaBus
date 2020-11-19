cf207faf652f94c54e24d34d2962fbdceddc5e57
noadahlia <ghozlan@g.jct.ac.il>
2020-11-19 22:08:32 +0100

    Before completing the main menue

--- dotNet5781_02_7799_9212/Program.cs
+++ dotNet5781_02_7799_9212/Program.cs
@@ -8,7 +8,6 @@ namespace dotNet5781_02_7799_9212
 {
     class Program
     {
-        enum zone  { General , North, South, Center, Jerusalem };
 
         static void Main(string[] args)
         {
@@ -27,7 +26,7 @@ namespace dotNet5781_02_7799_9212
                     case 1:
                         Console.WriteLine("Press 1 to add a new line or 2 to add a new station bus line");
                         int choice = int.Parse(Console.ReadLine());
-                        while(choice != 1 || choice != 2)
+                        while(choice != 1 && choice != 2)
                         {
                             Console.WriteLine("ERROR. Please try again. ");
                             goto case 1;
@@ -36,13 +35,25 @@ namespace dotNet5781_02_7799_9212
                         {
                             Console.WriteLine("Enter the bus line number");
                             int bnum = int.Parse(Console.ReadLine());
-                            //Console.WriteLine("Enter the first station of the bus:");
-                            //string bfirst = Console.ReadLine();
-                            //Console.WriteLine("Enter the last station of the bus");
-                            //string blast = Console.ReadLine();
-                            Console.WriteLine("Enter the area (General/North/South/Center/Jerusalem) :");
-                            string barea = Console.ReadLine();
-                            //BusLine newLine = new BusLine(bnum, bfirst, blast, barea);
+
+                        area:
+                            Console.WriteLine("Enter the area (General(1)/North(2)/South(3)/Center(4)/Jerusalem(5)) :");
+                            int barea = int.Parse(Console.ReadLine());
+                            if (barea<0 || barea>5){
+                                Console.WriteLine("This area is wrong. Please try again.");
+                                goto area;
+                            }
+
+                            Console.WriteLine("Add a new departure station:");
+                            Console.WriteLine("");
+
+                            
+                        }
+                        else if (choice == 2)
+                        {
+                            Console.WriteLine("Enter the bus line number");
+                            int tmp = int.Parse(Console.ReadLine());
+
                         }
 
                         break;
@@ -71,35 +82,7 @@ namespace dotNet5781_02_7799_9212
                 caseSwitch = int.Parse(Console.ReadLine());
 
             }
-            //BuStation station1;
-            //Console.WriteLine("Enter the bus station key");
-            //int key = int.Parse(Console.ReadLine());
-            //while (key.ToString().Length != 6)
-            //{
-            //    Console.WriteLine("ERROR. Enter a number with 6 digits:");
-            //    key = int.Parse(Console.ReadLine());
-            //}
-            //Console.WriteLine("Enter the latitude position of the station");
-            //double latitude = double.Parse(Console.ReadLine());
-            //while (latitude < 31 || latitude>33.3)
-            //{
-            //    Console.WriteLine("ERROR. Enter a number between 31 and 33.3:");
-            //    latitude = double.Parse(Console.ReadLine());
-            //}
-            //Console.WriteLine("Enter the longitude position of the station");
-            //double longitude = double.Parse(Console.ReadLine());
-            //while (longitude < 34.3 || longitude < 35.5) 
-            //{
-            //    Console.WriteLine("ERROR. Enter a number between 34.3 and 35.5:");
-            //    longitude = double.Parse(Console.ReadLine());
-            //}
-            //Console.WriteLine("Enter the name of the station");
-            //string statioName= Console.ReadLine();
-
-            //station1 = new BusLineStation(key, latitude, longitude, statioName, );
-            //BusLineStation x;
-            //int y = 10, c = 9;
-            //x = new BusLineStation(key, y);
+           
         }
 
     }
